baa177f2427ef7b861d08affe1774eedcbc99916
mnunes <matheusnunesinfo@gmail.com>
2023-03-15 18:22:20 -0300

    inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,52 @@
-import "./App.css";
+import {
+  Container,
+  Box,
+  Checkbox,
+  Typography,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <div className="card">
-        <span>Configuração jornada de trabalho</span>
-        <input
-          type="checkbox"
-          name="ativar horário de trabalho"
-          id="horario-de-trabalho"
-        />
-        <span>Configuração jornada de trabalho</span>
-      </div>
-    </div>
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={8} sx={{ margin: "auto" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Typography>Configuração jornada de trabalho</Typography>
+            <Typography>
+              <Checkbox />
+              Ativar horário de trabalho
+            </Typography>
+
+            <InputLabel id="jornadaTrabalho">
+              Configuração jornada de trabalho
+            </InputLabel>
+            <Select size="small" labelId="jornadaTrabalho" onChange={() => {}}>
+              <MenuItem value={1}>Abortar</MenuItem>
+              <MenuItem value={2}>Enviar no próximo expediente</MenuItem>
+            </Select>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
