e1f9df6a72bca82801eeff4209a2fc2fc137b2d9
mnunes <matheusnunesinfo@gmail.com>
2023-03-15 17:12:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <span>Configuração jornada de trabalho</span>
+        <input
+          type="checkbox"
+          name="ativar horário de trabalho"
+          id="horario-de-trabalho"
+        />
+        <span>Configuração jornada de trabalho</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
